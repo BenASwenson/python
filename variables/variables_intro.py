40b8baf2e4e6709c445163fb256935d3c314b767
BenjaminASwenson <BSwenson@spartaglobal.com>
2022-04-08 11:29:19 +0100

    looked at numeric types

---
+++ variables/variables_intro.py
@@ -0,0 +1,30 @@
+a = 1
+b = 2
+c = 3.5
+d = "hello"
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(a+c)
+
+# dynamically typed language
+
+print(type(a))
+print(type(d))
+print(type(c))
+
+# Numeric types
+
+a = 125 # int
+b = 1.25 # float
+c = 3 + 4j # complex
+
+
+print(c)
+print(type(c))
+
+print (a + b)
+print()
+
